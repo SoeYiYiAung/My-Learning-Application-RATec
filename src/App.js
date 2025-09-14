ddeea0caa8973935e7b6f2c0b7ab0ac694079b35
Soe Yi Yi Aung <76239004+SoeYiYiAung@users.noreply.github.com>
2025-09-14 21:29:40 +0630

    Some Testing Codes are added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ class Item extends React.Component {
         {this.props.name},
         ${this.props.price}
       </li>
+      
     );
+  }
+}
+
+class Fruit extends React.Component{
+  render(){
+    return(
+      <li>{this.props.name}</li>
+    )
   }
 }
 
@@ -15,11 +24,15 @@ class App extends React.Component {
     items: [
       { id: 1, name: 'Apple', price: 0.99 },
       { id: 2, name: 'Orange', price: 0.89 },
-    ]
+    ],
+    fruits: [],
+    mynum : 0,
   }
 
   nameRef = React.createRef();
   priceRef = React.createRef();
+  fruitRef = React.createRef();
+
   add = () => {
     let id = this.state.items.length + 1;
     let name = this.nameRef.current.value;
@@ -28,26 +41,52 @@ class App extends React.Component {
       items: [
         ...this.state.items,
         { id, name, price }
-      ]
+      ],
+
+      mynum: this.state.mynum + 1 
     });
   }
 
+  addFruit = () => {
+    let id = this.state.fruits.length + 1;
+    let name = this.fruitRef.current.value;
 
+    this.setState({
+      fruits: [...this.state.fruits, { id, name }],
+    });
+  };
 
   render() {
     return (
       <div>
         <h1>Hello React</h1>
         <ul>
-        {this.state.items.map(i => {
-        return (
-        <Item id={i.id} name={i.name} price={i.price} />
-        )
-        })}
+          {
+            this.state.items.map(i => {
+              return (
+                <Item id={i.id} name={i.name} price={i.price} />
+              )
+            })
+          }
         </ul>
+
+        <ol>
+          {this.state.fruits.map((f) => (
+            <Fruit key={f.id} id={f.id} name={f.name} />
+          ))}
+        </ol>
+        
+        <h3>My Number is {this.state.mynum}</h3>
+
         <input type="text" ref={this.nameRef} /><br />
         <input type="text" ref={this.priceRef} /><br />
         <button onClick={this.add}>Add</button>
+
+        <br /><br />
+
+        <input type="text" placeholder="Fruit name" ref={this.fruitRef} /><br />
+        <button onClick={this.addFruit}>Add Fruit</button>
+        
       </div>
 
     )
